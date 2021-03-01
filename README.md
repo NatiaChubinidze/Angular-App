366ffec363bd71363c8866ac083bbfed0701b1f7
LENOVO <chubinidze.natia97@gmail.com>
2021-03-01 18:03:30 +0400

    readme.md

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 *********************
 My First Angular App
 *********************
+Project link on Netlify - https://clever-einstein-8da620.netlify.app
--- src/app/Pipes/pipes.ts
+++ src/app/Pipes/pipes.ts
@@ -1,14 +1,13 @@
-import {Pipe,PipeTransform} from '@angular/core';
+import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-    name:'capitalize'
+  name: 'capitalize',
 })
-
-export class capitalize implements PipeTransform{
-    capitalize(str:string):string{
-return str.charAt(0).toUpperCase()+str.slice(1);
-    }
-    transform(value:string){
- return value.split(".").map(this.capitalize).join(".");
-    }
+export class capitalize implements PipeTransform {
+  capitalize(str: string): string {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+  transform(value: string) {
+    return value.split('.').map(this.capitalize).join('.');
+  }
 }
